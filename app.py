52f26a5d1f29de16087e21a32a30df809dd0925f
gbnk0 <michael.amara2@gmail.com>
2018-07-20 19:14:35 +0200

    Add get and insert dataset

--- app.py
+++ app.py
@@ -11,32 +11,44 @@ def make_dir(directory):
         print("Created '{}' directory.".format(directory))
 
 def create_tables():
-    cursor = app.db.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS datasets(id INTEGER PRIMARY KEY, name TEXT,
-                                path TEXT)
+    c = app.db.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS datasets(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE)
     ''')
     app.db.commit()
 
-# class Datasets(object):
+def get_datasets():
+    c = app.db.cursor()
+    for row in c.execute('SELECT * FROM datasets ORDER BY name'):
+            print(row)
 
-#     def __init__(self, *args):
- 
+def insert_dataset(name):
+    result = False
+    c = app.db.cursor()
+    try:
+        c.execute("INSERT INTO datasets(name) VALUES ('{}')".format(name))
+        app.db.commit()
+        result = True
+    except Exception as e:
+        print(e)
+    return result
 
 
 @app.post('/label')
 async def route_label_picture(request):
     return json({'data': 'a smartphone'})
 
-@app.post('/new/dataset')
+@app.put('/dataset')
 async def route_new_dataset(request):
-    if len(request.json) > 0:
-        d = Dataset(request.json.name)
+    # if len(request.json) > 0:
+        # d = insert_dataset(request.json.name)
     return json({'status': 'created'})
 
 if __name__ == '__main__':
     make_dir('data/')
     app.db = sqlite3.connect('data/classifier.db')
     create_tables()
-    app.run(host='0.0.0.0', port=8000)
+    insert_dataset('test1')
+    get_datasets()
+    app.run(host='0.0.0.0', port=8080)
     app.db.close()
